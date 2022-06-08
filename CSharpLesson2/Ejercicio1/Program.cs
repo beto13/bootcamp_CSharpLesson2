5df8c0b254358ba72d4155833a14f81a3649c6c2
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-08 19:47:22 -0300

    commit ejercicio 1

--- CSharpLesson2/Ejercicio1/Program.cs
+++ CSharpLesson2/Ejercicio1/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Por favor ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su apellido: ");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+string mensaje = $"Hola " + nombre +apellido;
+
+Console.WriteLine(mensaje);
+
+Console.ReadKey();
