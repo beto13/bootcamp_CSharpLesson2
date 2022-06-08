24d8ed57bb062cf6d9ebbf553eac8b6f232f5e71
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-08 19:54:08 -0300

    commit ejercicio 2

---
+++ CSharpLesson2/Ejercici2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string Coche ;
+int Puertas ;
+int Ruedas ;
+string Marca ;
+string ITV ;
+bool Vigente ;
+
+string Mesa;
+float Peso;
+float Largo;
+string Material;
+string Color;
+
