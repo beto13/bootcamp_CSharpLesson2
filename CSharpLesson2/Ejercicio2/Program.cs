0ba35739af1e1405a28f972645d8db7b321ce91b
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-08 19:59:21 -0300

    commit ejercicio 3

---
+++ CSharpLesson2/Ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numero = 5;
+if ( numero >= 18)
+
+char chr = 'a';
+if (chr == 'a')
+
+
+if (chr == 'a' && numero == 5)
+
+if (chr == 'a' && numero >= 18)
